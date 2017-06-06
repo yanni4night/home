807b54ae3ff0f68b5d4fe69f8c69a4dde43525b6
yinyong <yanni4night@gmail.com>
2017-06-06 11:33:15 +0800

    update

--- custom_scripts/git.sh
+++ custom_scripts/git.sh
@@ -2,7 +2,7 @@
 
 #branch=`git br | awk '{if($1=="*"){print $2}}'`
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
+cmd="$1"
 opt='push'
 
 if [ $branch = '' ];then
@@ -10,12 +10,17 @@ if [ $branch = '' ];then
   exit
 fi
 
-if [ "$1" != 'push' ] && [ "$1" != 'pull' ];then
+if [ "$cmd" != 'push' ] && [ "$cmd" != 'pushr' ] && [ "$cmd" != 'pull' ];then
     echo "\033[0;31mYou can do push or pull,not \"$1\"\033[0m"
     exit
 else
-    opt=$1
+    opt=$cmd
 fi
 
-echo "\033[0;32mgit $opt origin $branch\033[0m"
-git $opt origin $branch --tag
+if [ "$cmd" = 'pushr' ];then
+    echo "\033[0;32mgit $opt origin HEAD:refs/for/$branch\033[0m"
+    git push origin HEAD:refs/for/$branch
+else
+    echo "\033[0;32mgit $opt origin $branch\033[0m"
+    git $opt origin $branch --tag
+fi
